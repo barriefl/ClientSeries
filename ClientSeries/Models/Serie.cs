10176343cff1951f80fff20dd808cd889ffc6057
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-02-14 11:46:36 +0100

    Codage WSService, pas terminé.

---
+++ ClientSeries/Models/Serie.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClientSeries.Models;
+
+[Table("serie")]
+public partial class Serie
+{
+    [Key]
+    [Column("serieid")]
+    public int Serieid { get; set; }
+
+    [Column("titre")]
+    [StringLength(100)]
+    public string Titre { get; set; } = null!;
+
+    [Column("resume")]
+    public string? Resume { get; set; }
+
+    [Column("nbsaisons")]
+    public int? Nbsaisons { get; set; }
+
+    [Column("nbepisodes")]
+    public int? Nbepisodes { get; set; }
+
+    [Column("anneecreation")]
+    public int? Anneecreation { get; set; }
+
+    [Column("network")]
+    [StringLength(50)]
+    public string? Network { get; set; }
+
+    public Serie(int serieid, string titre, string? resume, int? nbsaisons, int? nbepisodes, int? anneecreation, string? network)
+    {
+        Serieid = serieid;
+        Titre = titre;
+        Resume = resume;
+        Nbsaisons = nbsaisons;
+        Nbepisodes = nbepisodes;
+        Anneecreation = anneecreation;
+        Network = network;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Serie serie &&
+               Serieid == serie.Serieid &&
+               Titre == serie.Titre &&
+               Resume == serie.Resume &&
+               Nbsaisons == serie.Nbsaisons &&
+               Nbepisodes == serie.Nbepisodes &&
+               Anneecreation == serie.Anneecreation &&
+               Network == serie.Network;
+    }
+}
---
+++ ClientSeries/Services/WSService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using ClientSeries.Services;
+using ClientSeries.Models;
+using System.Net.Http.Json;
+
+namespace ClientSeries.Services
+{
+    public class WSService : IService
+    {
+        private HttpClient client = new HttpClient();
+
+        public WSService(string url)
+        {
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<List<Serie>> GetSeriesAsync(string nomController)
+        {
+            try
+            {
+                return await client.GetFromJsonAsync<List<Serie>>(nomController);
+            }
+            catch (Exception ex) 
+            {
+                return null;
+            }
+        }
+
+        public async Task<Serie> GetSerieAsync(string nomController, Serie serie)
+        {
+            try
+            {
+                return await client.GetFromJsonAsync<Serie>(nomController, serie);
+            }
+            catch (Exception ex) 
+            {
+                return null;
+            }
+        }
+
+        public async Task<Serie> PutSerieAsync(string nomController, Serie serie)
+        {
+            try
+            {
+                var reponse = await client.PutAsJsonAsync(nomController, serie);
+                return await reponse.Content.ReadFromJsonAsync<Serie>();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async Task<Serie> PostSerieAsync(string nomController, Serie serie)
+        {
+            try
+            {
+                var reponse = await client.PostAsJsonAsync(nomController, serie);
+                return await reponse.Content.ReadFromJsonAsync<Serie>();
+            }
+            catch (Exception ex) 
+            {
+                return null;
+            }
+        }
+
+        public async Task<Serie> DeleteSerieAsync(string nomController, Serie serie)
+        {
+            try
+            {
+                var reponse = await client.DeleteFromJsonAsync(nomController);
+                
+            }
+            catch (Exception ex) 
+            {
+                return null;
+            }
+        }
+    }
+}
